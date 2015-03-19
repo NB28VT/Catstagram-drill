21c03e733c123244d5542e52a41ef4181bbfb7fd
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-19 14:26:50 -0400

    Passing unit tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "#password" do
+      it { should have_valid(:password).when("abcd1234"), "asd^2jk@%111"}
+      it { should_not have_valid(:password).when("abcd123", nil, "")}
+    end
+
+    describe "#password_confirmation" do
+      subject { FactoryGirl.build(:user, password: "abcd1234")}
+      it { should have_valid(:password_confirmation).when("abcd1234")}
+      it {should_not have_valid(:password_confirmation).when("asdas") }
+    end
+
+    describe "#email" do
+      subject { FactoryGirl.create(:user) }
+      it { should_not have_valid(:email).when("wasd", "kitty.com", "Ki@", nil, "")}
+      it { should validate_uniqueness_of(:email) }
+    end
 end
