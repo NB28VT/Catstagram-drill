a8aa7eba8303142599b14bdedbdfe0f4c4c82661
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-20 14:32:21 -0400

    Added indexed meows table

--- db/migrate/20150320182810_create_meows.rb
+++ db/migrate/20150320182810_create_meows.rb
@@ -1,10 +1,11 @@
 class CreateMeows < ActiveRecord::Migration
   def change
     create_table :meows do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
 
       t.timestamps null: false
     end
+    add_index :meows, [:post_id, :user_id], unique: true
   end
 end
