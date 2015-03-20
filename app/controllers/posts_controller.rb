57cc8d6658e47654b62e980c57569868833c97c7
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-20 12:29:56 -0400

    Completed posting feature with tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
 
   def index
-    @posts = Post.all
+    @posts = Post.by_recency
   end
 
 
