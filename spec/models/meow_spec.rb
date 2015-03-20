90f59a4ca118c2c95e5761fb2a9945979233960d
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-20 14:28:27 -0400

    Added meow model

---
+++ spec/models/meow_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Meow do
+  before :each { FactoryGirl.create(:meow) }
+
+  it { should belong_to :user }
+  it { should belong_to :post }
+
+  it { should have_valid(:user).when(User.new) }
+  it { should_not have_valid(:user).when(nil) }
+
+  it { should have_valid(:post).when(Post.new) }
+  it { should_not have_valid(:post).when(nil) }
+
+  it { should validate_uniqueness_of(:post_id).scoped_to(:user_id)
+  }
+end
